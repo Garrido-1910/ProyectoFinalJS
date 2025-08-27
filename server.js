dfded20f65995e6f17f8534e80f1d14df5424d59
Christopher Garrido <c.garridofwd19@gmail.com>
2025-08-27 10:01:09 -0600

    avance

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'pages/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'pages/solicitud.html'));
 });
 
 const PORT = 3000;
