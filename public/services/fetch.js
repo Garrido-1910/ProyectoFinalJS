4eebd0a75bf7f521b996de3ddd35bfd3d8045891
Christopher Garrido <c.garridofwd19@gmail.com>
2025-08-29 15:14:36 -0600

    commit

--- public/services/fetch.js
+++ public/services/fetch.js
@@ -25,5 +25,21 @@ async function getData(endpoint) {
     return [];
   }
 }
+async function patchData(obj,endpoint,id) {
+  try {
+      const peticion = await fetch(`http://localhost:3001/${endpoint}/${id}`,{
+          method: 'PATCH',
+          headers:{
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(obj)
+      })
+      const respuesta = await peticion.json()
+      console.log(respuesta);
+      return respuesta
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-export {postData,getData}
+export {postData,getData,patchData}
