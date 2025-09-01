a2551bc8fab7723a1e035afefc5311fb5e80e7ae
Christopher Garrido <c.garridofwd19@gmail.com>
2025-09-01 11:28:51 -0600

    commit

--- public/js/restablecer.js
+++ public/js/restablecer.js
@@ -10,38 +10,39 @@ function generateToken() {
 }
 
 /* Envía el correo de recuperación con el token */
-function sendRecovery() {
-  const email = document.getElementById("email").value;
 
+async function sendRecovery() {
+  const email = document.getElementById("email").value;
   if (!email) {
     alert("Por favor ingresa tu correo");
     return;
   }
-  const usuariosRegistrados = await getData("usuarios")
-
-  const estaRegistrado = usuariosRegistrados.some((correoUsuario)=>correoUsuario.correo === email)
-
-  if (!estaRegistrado) {
-      alert("Cuenta no registrada")
-      return
+  let usuariosRegistrados = [];
+  try {
+    usuariosRegistrados = await getData("usuarios");
+  } catch {
+    alert("Error al consultar usuarios");
+    return;
   }
-
+  const estaRegistrado = usuariosRegistrados.some((correoUsuario) => correoUsuario.correo === email);
+  if (!estaRegistrado) {
+    alert("Cuenta no registrada");
+    return;
+  }
   const token = generateToken();
-  const link = `aaaaa`; 
-  /* Envía el correo usando emailjs */
-  emailjs.send("service_mqp2a4u", "template_m0bnnew", {
-    to_email: email,
-    token: token,
-    link: link,
-    email: email
-  })
-  .then(() => {
+  const link = `aaaaa`;
+  try {
+    await emailjs.send("service_mqp2a4u", "template_m0bnnew", {
+      to_email: email,
+      token: token,
+      link: link,
+      email: email
+    });
     alert("¡Correo de recuperación enviado!");
-  })
-  .catch(err => {
+  } catch (err) {
     console.error("Error:", err);
     alert("Hubo un error al enviar el correo.");
-  });
+  }
 }
 
 document.getElementById("btnEnviar").addEventListener("click", sendRecovery);
