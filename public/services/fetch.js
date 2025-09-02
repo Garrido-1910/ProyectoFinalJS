278978f0062232e8c6ba92f210a13c7bdaaae463
Jmendez18 <josue.munguia1607fwd@gmail.com>
2025-09-02 08:18:40 -0600

    newnew-css

--- public/services/fetch.js
+++ public/services/fetch.js
@@ -16,7 +16,7 @@ async function postData(obj,endpoint) {
 }
 async function getData(endpoint) {
   try {
-    const peticion = await fetch(`http://localhost:3001/${endpoint}`);
+    const peticion = await fetch(`http://localhost:3000/${endpoint}`);
     const respuesta = await peticion.json();
     console.log(respuesta);
     return respuesta;
@@ -27,7 +27,7 @@ async function getData(endpoint) {
 }
 async function patchData(obj,endpoint,id) {
   try {
-      const peticion = await fetch(`http://localhost:3001/${endpoint}/${id}`,{
+      const peticion = await fetch(`http://localhost:3000/${endpoint}/${id}`,{
           method: 'PATCH',
           headers:{
               'Content-Type': 'application/json'
